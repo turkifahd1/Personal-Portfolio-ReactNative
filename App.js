5fb3e4cd1bc0e1947edd87b394ad5b7d97b6fd52
turkifahd1 <jjbh2710@gmail.com>
2024-06-13 08:39:24 +0300

    abdatee

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import  React,{useState} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 
-import MyTabs from './pages/tab/Tab';
+import MyTabs from './src/pages/tab/Tab';
 
 
 
